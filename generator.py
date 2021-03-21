a0be7477154403a6e3f47ba2c1f3ebe406a2888b
Varun Kumar <heysadboy@gmail.com>
2021-03-21 11:35:31 +0900

    update generator file

--- generator.py
+++ generator.py
@@ -7,7 +7,7 @@ import json
 
 root = "https://api.challenge.hennge.com/challenges/003"
 content_type = "application/json"
-userid = "heysadboy@gmail.com"
+userid = "YOUR_EMAIL_ID"
 secret_suffix = "HENNGECHALLENGE003"
 shared_secret = userid+secret_suffix
 
@@ -15,27 +15,17 @@ timestep = 30
 T0 = 0
 
 def HOTP(K, C, digits=10):
-    """HTOP:
-    K is the shared key
-    C is the counter value
-    digits control the response length
-    """
     K_bytes = str.encode(K)
     C_bytes = struct.pack(">Q", C)
     hmac_sha512 = hmac.new(key = K_bytes, msg=C_bytes, digestmod=hashlib.sha512).hexdigest()
     return Truncate(hmac_sha512)[-digits:]
 
 def Truncate(hmac_sha512):
-    """truncate sha512 value"""
     offset = int(hmac_sha512[-1], 16)
     binary = int(hmac_sha512[(offset *2):((offset*2)+8)], 16) & 0x7FFFFFFF
     return str(binary)
 
 def TOTP(K, digits=10, timeref = 0, timestep = 30):
-    """TOTP, time-based variant of HOTP
-    digits control the response length
-    the C in HOTP is replaced by ( (currentTime - timeref) / timestep )
-    """
     C = int ( time.time() - timeref ) // timestep
     return HOTP(K, C, digits = digits)
 
